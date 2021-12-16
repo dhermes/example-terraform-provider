3d6d5756acac6684a0880c18de52e4a4c393b435
Danny Hermes <daniel.j.hermes@gmail.com>
2021-12-16 09:45:49 -0600

    Pre-emptive rename for `books` package.

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -18,13 +18,13 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/dhermes/example-terraform-provider/pkg/books"
+	providershim "github.com/dhermes/example-terraform-provider/pkg/books"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
-			return books.Provider()
+			return providershim.Provider()
 		},
 	})
 }
